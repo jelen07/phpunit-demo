86660c1f8369092ac9a0f7923ab2b0299f17343b
Vojtěch Lacina <vojtech.lacina01@currys.co.uk>
2023-05-26 07:28:06 +0200

    Add more testing data

--- tests/unit/Calculator.add.test.php
+++ tests/unit/Calculator.add.test.php
@@ -4,11 +4,43 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Demo\Calculator;
 
-$calculator = new Calculator();
-$result = $calculator->add(1, 2);
+$data = [
+    3 => [
+        'a' => 1,
+        'b' => 2,
+    ],
+    0 => [
+        'a' => -1,
+        'b' => 1,
+    ],
+    1 => [
+        'a' => 0,
+        'b' => 1,
+    ],
+    1000 => [
+        'a' => 999,
+        'b' => 1,
+    ],
+    999 => [
+        'a' => 1000,
+        'b' => -1,
+    ],
+    4 => [
+        'a' => 2,
+        'b' => 2,
+    ],
+];
 
-if ($result !== 3) {
-    throw new \RuntimeException("1 + 2 should equal 3, $result returned 🔥");
+$calculator = new Calculator();
+foreach ($data as $expected => $input) {
+    try {
+        $result = $calculator->add(...$input);
+    } catch (\RuntimeException $e) {
+        $result = \RuntimeException::class;
+    }
+    if ($result !== $expected) {
+        throw new \RuntimeException("{$input['a']} + {$input['b']} should equal $expected, $result returned 🔥");
+    }
 }
 
 echo "All tests passed! 🍺 in " . __FILE__ . PHP_EOL;
