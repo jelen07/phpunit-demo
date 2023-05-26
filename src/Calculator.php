b8b97e7dc6966bbedba0bb4c1d07fe5844a98423
Vojtěch Lacina <vojtech.lacina01@currys.co.uk>
2023-05-26 07:20:15 +0200

    Add Calculator class
    - And Composer init

---
+++ src/Calculator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Demo;
+
+class Calculator
+{
+    public function add($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+
+    public function divide($a, $b) {
+        if ($b !== 0) {
+            return $a / $b;
+        } else {
+            throw new \RuntimeException("Cannot divide by zero");
+        }
+    }
+}
